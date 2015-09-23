dd4d20e9a02049ad661e7cae49fff004c1a963f3
Joe Lauer <joe@lauer.bz>
2015-09-23 19:50:23 -0400

    Initial working version

--- module/src/main/java/com/fizzed/ninja/rocker/NinjaRockerTemplate.java
+++ module/src/main/java/com/fizzed/ninja/rocker/NinjaRockerTemplate.java
@@ -4,6 +4,7 @@ package com.fizzed.ninja.rocker;
 import com.fizzed.rocker.RenderingException;
 import com.fizzed.rocker.RockerOutput;
 import com.fizzed.rocker.runtime.ArrayOfByteArraysOutput;
+import com.fizzed.rocker.runtime.DefaultRockerModel;
 import com.fizzed.rocker.runtime.DefaultRockerTemplate;
 import java.io.File;
 import java.io.IOException;
@@ -27,156 +28,21 @@ import org.slf4j.LoggerFactory;
 public abstract class NinjaRockerTemplate<T extends NinjaRockerTemplate> extends DefaultRockerTemplate<T> {
     static private final Logger log = LoggerFactory.getLogger(NinjaRockerTemplate.class);
     
-    protected NinjaRocker N;
+    public NinjaRocker N;
     
-    /**
-     * Configure this template from another template.  Most important is that
-     * the "implicit" variables from the other template are copied into this
-     * template.
-     * @param <T>
-     * @param other
-     * @throws RenderingException 
-     */
-    @Override
-    protected <T> void __configure(T other) throws RenderingException {
-        super.__configure(other);
-        if (other instanceof NinjaRockerTemplate) {
-            NinjaRockerTemplate otherTemplate = (NinjaRockerTemplate)other;
-            // share the ninja+rocker context
-            this.N = otherTemplate.N;
-        } else {
-            throw new RenderingException("Unable to configure template (not an instance of " + this.getClass().getName() + ")");
-        }
+    public NinjaRockerTemplate(DefaultRockerModel model) {
+        super(model);
     }
     
     @Override
-    protected RockerOutput __newOutput() {
-        // output optimized for array of byte arrays...
-        return new ArrayOfByteArraysOutput(__internal.getCharset());
-    }
-    
-    /**
-     * Implements <code>Renderable</code> for Ninja result so that this template
-     * can be directly used in a Result. Not intended to be called by application
-     * directly.
-     * 
-     * @param ninjaRockerContext  The ninja+rocker context
-     * @param context The context of the request
-     * @param result The result we are rendering for
-     */
-    public void ninjaRender(NinjaRockerContext ninjaRockerContext, Context context, Result result) {
-        
-        this.N = new NinjaRocker(ninjaRockerContext, context, result);
-   
-        // set content type if not set
-        if (result.getContentType() == null) {
-            switch (this.__internal.getContentType()) {
-                case HTML:
-                    result.html();
-                    break;
-                case RAW:
-                    result.contentType(Result.APPLICATION_OCTET_STREAM);
-                    break;
-            }
+    protected void __associate(DefaultRockerTemplate context) throws RenderingException {
+        super.__associate(context);
+        if (context instanceof NinjaRockerTemplate) {
+            NinjaRockerTemplate ninjaTemplate = (NinjaRockerTemplate)context;
+            this.N = ninjaTemplate.N;
         }
-        
-        // looks like jetty gives us a single shot to write out the bytes
-        // to the underlying output stream -- chunked results could be more
-        // interesting to look at down the road
-        RockerOutput out = null;
-        
-        try {
-            // try to render output
-            out = this.render();
-        } catch (RenderingException e) {
-            
-            throwRenderingException(context, result, e);
-            
-            //handleRenderingException(context, result, e);
-            //log.error(e.getMessage(), e);
-            //return
-        }
-        
-        ArrayOfByteArraysOutput abao = (ArrayOfByteArraysOutput)out;
-        
-        // rendering was successful, finalize headers, and write it to output
-        ResponseStreams responseStreams = context.finalizeHeaders(result);
-        
-        try (OutputStream os = responseStreams.getOutputStream()) {
-            os.write(abao.toByteArray());
-            os.flush();
-            os.close();
-        } catch (IOException e) {
-            throw new InternalServerErrorException(e);
+        else {
+            throw new RenderingException("Template was not type " + NinjaRockerTemplate.class.getCanonicalName());
         }
     }
-    
-    public void throwRenderingException(
-            Context context,
-            Result result,
-            RenderingException cause) {
-        
-        // likely project source code
-        String sourcePath = null;
-        if (this.__internal.getTemplatePackageName() != null
-                && this.__internal.getTemplateName() != null) {
-            sourcePath = this.__internal.getTemplatePackageName().replace(".", File.separator)
-                    + File.separator
-                    + this.__internal.getTemplateName();
-        }
-        
-        /**
-        throw new ninja.exceptions.RenderingException(
-                cause.getMessage(),
-                cause,
-                result,
-                "Rocker rendering exception",
-                sourcePath,
-                cause.getSourceLine());
-        */
-        
-        throw TemplateEngineRocker.renderingOrRuntimeException(
-                cause.getMessage(),
-                cause,
-                result,
-                "Rocker rendering exception",
-                sourcePath,
-                cause.getSourceLine()
-        );
-    }
-    
-    /**
-    private void handleRenderingException(Context context, Result result, RenderingException renderingException) {
-        ///////////////////////////////////////////////////////////////////////
-        // In dev mode full stack trace is shown for exception during rendering
-        // In test & prod mode we delegate to normal exception handler
-        ///////////////////////////////////////////////////////////////////////      
-        if (__ninjaProperties.isDev()) {
-            //
-            // try to extract source snippet from template
-            //
-            int lineNumberOfError = renderingException.getSourceLine();
-            int lineFrom = lineNumberOfError - 4;
-            int lineTo = lineNumberOfError + 5;
-            
-            SourceSnippetHelper.SourceSnippet snippet = null;
-            
-            DiagnosticError diagnosticError
-                = DiagnosticErrorHelper.buildDiagnosticError(
-                    context,
-                    result,
-                    "Rocker template rendering exception",
-                    renderingException,
-                    snippet,
-                    lineNumberOfError);
-            
-            DiagnosticErrorRenderer der = DiagnosticErrorRenderer.build(diagnosticError);
-            
-            der.tryToRenderResult(context, result, log, true);
-        } else {
-            // fallback to ninja system-wide error handler 
-            throw new InternalServerErrorException(renderingException);
-        }
-    }
-    */
 }
--- module/src/main/java/com/fizzed/ninja/rocker/TemplateEngineRocker.java
+++ module/src/main/java/com/fizzed/ninja/rocker/TemplateEngineRocker.java
@@ -17,8 +17,13 @@
 package com.fizzed.ninja.rocker;
 
 import com.fizzed.ninja.rocker.views.common_error;
+import com.fizzed.rocker.RenderingException;
+import com.fizzed.rocker.RockerModelCallback;
+import com.fizzed.rocker.RockerOutput;
+import com.fizzed.rocker.RockerRuntime;
 import com.fizzed.rocker.RockerTemplate;
-import com.fizzed.rocker.runtime.DefaultRockerTemplate;
+import com.fizzed.rocker.runtime.ArrayOfByteArraysOutput;
+import com.fizzed.rocker.runtime.DefaultRockerModel;
 import java.util.Map;
 
 import javax.inject.Singleton;
@@ -30,14 +35,17 @@ import ninja.utils.NinjaProperties;
 import org.slf4j.Logger;
 
 import com.google.inject.Inject;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Collections;
 import java.util.HashMap;
+import ninja.exceptions.InternalServerErrorException;
 import ninja.template.TemplateEngine;
 import ninja.template.TemplateEngineHelper;
 import ninja.utils.Message;
 import ninja.utils.NinjaConstant;
+import ninja.utils.ResponseStreams;
 import org.slf4j.LoggerFactory;
 
 @Singleton
@@ -50,7 +58,7 @@ public class TemplateEngineRocker implements TemplateEngine {
     private final NinjaRockerContext ninjaRockerContext;
     private final String fileSuffix;
     private final String contentType;
-    private final Map<String, Class<? extends NinjaRockerTemplate>> commonErrorTemplates;
+    private final Map<String, Class<? extends DefaultRockerModel>> commonErrorTemplates;
     
     @Inject
     public TemplateEngineRocker(NinjaRockerContext ninjaRockerContext,
@@ -68,39 +76,44 @@ public class TemplateEngineRocker implements TemplateEngine {
         this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_NOT_FOUND, common_error.class);
         this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_UNAUTHORIZED, common_error.class);
  
+        if (ninjaProperties.isDev()) {
+            // try to set reloading to true in dev mode
+            RockerRuntime.getInstance().setReloading(true);
+        }
+        
         loadCustomErrorTemplates();
     }
     
     public final void loadCustomErrorTemplates() {
-        Class<NinjaRockerTemplate> internalServerErrorClass = getCustomErrorViewClass("views.system.internal_server_error");
+        Class<DefaultRockerModel> internalServerErrorClass = getCustomErrorViewClass("views.system.internal_server_error");
         if (internalServerErrorClass != null) {
             this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_INTERNAL_SERVER_ERROR, internalServerErrorClass);
         }
         
-        Class<NinjaRockerTemplate> badRequestClass = getCustomErrorViewClass("views.system.bad_request");
+        Class<DefaultRockerModel> badRequestClass = getCustomErrorViewClass("views.system.bad_request");
         if (badRequestClass != null) {
             this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_BAD_REQUEST, badRequestClass);
         }
         
-        Class<NinjaRockerTemplate> forbiddenClass = getCustomErrorViewClass("views.system.forbidden");
+        Class<DefaultRockerModel> forbiddenClass = getCustomErrorViewClass("views.system.forbidden");
         if (forbiddenClass != null) {
             this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_FORBIDDEN, forbiddenClass);
         }
         
-        Class<NinjaRockerTemplate> notFoundClass = getCustomErrorViewClass("views.system.not_found");
+        Class<DefaultRockerModel> notFoundClass = getCustomErrorViewClass("views.system.not_found");
         if (notFoundClass != null) {
             this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_NOT_FOUND, notFoundClass);
         }
         
-        Class<NinjaRockerTemplate> unauthorizedClass = getCustomErrorViewClass("views.system.unauthorized");
+        Class<DefaultRockerModel> unauthorizedClass = getCustomErrorViewClass("views.system.unauthorized");
         if (unauthorizedClass != null) {
             this.commonErrorTemplates.put(NinjaConstant.LOCATION_VIEW_FTL_HTML_UNAUTHORIZED, unauthorizedClass);
         }
     }
     
-    public Class<NinjaRockerTemplate> getCustomErrorViewClass(String className) {
+    public Class<DefaultRockerModel> getCustomErrorViewClass(String className) {
         try {
-            return (Class<NinjaRockerTemplate>)Class.forName(className);
+            return (Class<DefaultRockerModel>)Class.forName(className);
         } catch (Throwable t) {
             // do nothing
             return null;
@@ -125,15 +138,15 @@ public class TemplateEngineRocker implements TemplateEngine {
         
         // various types of objects renderable may store
         Message valueMessage = null;
-        NinjaRockerTemplate rockerTemplate = null;
+        DefaultRockerModel model = null;
         
         
         // if the object is null we simply render an empty map...
         if (object == null) {            
             throw renderingOrRuntimeException("Renderable was null. You must pass an instance of a NinjaRockerTemplate the Result.render() method.", null);
         }
-        else if (object instanceof NinjaRockerTemplate) {
-            rockerTemplate = (NinjaRockerTemplate)object;   
+        else if (object instanceof DefaultRockerModel) {
+            model = (DefaultRockerModel)object;   
         }
         else if (object instanceof RockerTemplate) {
             throw renderingOrRuntimeException("Only templates with a parent class type of NinjaRockerTemplate are supported by this engine. " +
@@ -148,35 +161,119 @@ public class TemplateEngineRocker implements TemplateEngine {
         // until NinjaFramework makes these template names configurable -- these
         // need to be handled by creating instances of correct Rocker template
         //
-        if (rockerTemplate == null) {
+        if (model == null) {
             String templateName = result.getTemplate();
             
             if (templateName != null && commonErrorTemplates.containsKey(templateName)) {
-                Class<? extends NinjaRockerTemplate> commonErrorType = commonErrorTemplates.get(templateName);
+                Class<? extends DefaultRockerModel> commonErrorType = commonErrorTemplates.get(templateName);
 
-                NinjaRockerTemplate rockerErrorTemplate = null;
+                DefaultRockerModel errorModel = null;
 
                 try {
-                    rockerErrorTemplate = commonErrorType.newInstance();
+                    errorModel = commonErrorType.newInstance();
                 } catch (InstantiationException | IllegalAccessException e) {
                     throw renderingOrRuntimeException("Unable to create template class " + commonErrorType, e);
                 }
 
-                if (rockerErrorTemplate instanceof CommonErrorTemplate) {
+                if (errorModel instanceof common_error) {
 
-                    ((CommonErrorTemplate)rockerErrorTemplate).message(valueMessage);
+                    ((common_error)errorModel).message(valueMessage);
 
                 }
 
-                rockerTemplate = rockerErrorTemplate;
+                model = errorModel;
             } else {
                 throw renderingOrRuntimeException("Renderable object was not of class type 'NinjaRockerTemplate'. Rather it was of type '" + object.getClass().getSimpleName() + "'. You must pass an instance of NinjaRockerTemplate to the Result.render() method.", null);
             }
         }
+        
+        // create the N variable
+        final NinjaRocker N = new NinjaRocker(ninjaRockerContext, context, result);
+        
+        // register callback so we can inject what we need into template
+        // prior to rendering, but after template was generated
+        model.__callback(new RockerModelCallback() {
+            @Override
+            public void onRender(RockerTemplate template) {
+                if (template instanceof NinjaRockerTemplate) {
+                    NinjaRockerTemplate ninjaTemplate = (NinjaRockerTemplate)template;
+                    ninjaTemplate.N = N;
+                }
+            }
+        });
+        
+        // looks like jetty gives us a single shot to write out the bytes
+        // to the underlying output stream
+        RockerOutput out = null;
+        
+        try {
+            out = model.render();
+        } catch (RenderingException e) {
+            throwRenderingException(context, result, e);
+        }
+        
+        
+        // set content type if not set
+        if (result.getContentType() == null) {
+            switch (out.getContentType()) {
+                case HTML:
+                    result.html();
+                    break;
+                case RAW:
+                    result.contentType(Result.APPLICATION_OCTET_STREAM);
+                    break;
+            }
+        }
 
-        // inject context for ninja into template, then delegate rendering to itself
-        rockerTemplate.ninjaRender(ninjaRockerContext, context, result);
+        
+        ArrayOfByteArraysOutput abao = (ArrayOfByteArraysOutput)out;
+        
+        // rendering was successful, finalize headers, and write it to output
+        ResponseStreams responseStreams = context.finalizeHeaders(result);
+        
+        try (OutputStream os = responseStreams.getOutputStream()) {
+            os.write(abao.toByteArray());
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            throw new InternalServerErrorException(e);
+        }
     }
+    
+    public void throwRenderingException(
+            Context context,
+            Result result,
+            RenderingException cause) {
+        
+        // likely project source code
+        String sourcePath = cause.getTemplatePath();
+        
+        /**
+        throw new ninja.exceptions.RenderingException(
+                cause.getMessage(),
+                cause,
+                result,
+                "Rocker rendering exception",
+                sourcePath,
+                cause.getSourceLine());
+        */
+        
+        throw TemplateEngineRocker.renderingOrRuntimeException(
+                cause.getMessage(),
+                cause,
+                result,
+                "Rocker rendering exception",
+                sourcePath,
+                cause.getSourceLine()
+        );
+    }
+    
+    
+    
+    
+    
+    
+    
     
     
     static public RuntimeException renderingOrRuntimeException(String message, Exception cause) throws RuntimeException {
